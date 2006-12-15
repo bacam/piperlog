7e9277fb33300e0dac145c7873f37b9b712d1e40
Brian Campbell <bacam@tardis.ed.ac.uk>
2006-12-15 23:36:19 +0000

    Add discard file name to be given on the command line
    darcs-hash:20061215233619-11e5d-ce59ba5d802977f51f42a6f0e9b710a251b4e1c8.gz

--- piperlog-convert.ml
+++ piperlog-convert.ml
@@ -2,13 +2,19 @@ let extracrap = "^\\w{3} [ :0-9]{11} [._[:alnum:]-]+ ";;
 let extralen = String.length extracrap;;
 let discard = Hashtbl.create 20;;
 
+let discardname = ref "/etc/piperlog/discard" in
+Arg.parse [
+  ("--discard", Arg.Set_string discardname, "File of patterns to discard")
+  ] (fun _ -> raise (Arg.Bad "Too many arguments"))
+    "Usage: piperlog-convert [--discard <file name>]";
+
 (* The logcheck files include patterns to match the date, time and host,
    so we need to strip that off.
 
    The "discard" file is in our format.
  *)
 
-File.foldfile "discard" (fun () l -> Hashtbl.add discard l ()) ();;
+File.foldfile !discardname (fun () l -> Hashtbl.add discard l ()) ();;
 
 try
 while true do
